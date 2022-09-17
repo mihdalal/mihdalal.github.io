1538344aaf50a41eef90e639730031ed8546492e
Murtaza Dalal <murtaza@Murtazas-MacBook-Pro.local>
2022-09-17 19:10:39 -0400

    visual cleanup

--- main.js
+++ main.js
@@ -22,25 +22,6 @@ function parseQuery(query) {
     return Params;
 }
 
-// function showPubs(id) {
-//     if (id == 0) {
-//       document.getElementById('pubs').innerHTML = document.getElementById('pubs_selected').innerHTML;
-//       document.getElementById('select0').style = 'text-decoration:underline;color:#000000';
-//       document.getElementById('select1').style = '';
-//       document.getElementById('select2').style = '';
-//     } else if (id == 1) {
-//       document.getElementById('pubs').innerHTML = document.getElementById('pubs_by_date').innerHTML;
-//       document.getElementById('select1').style = 'text-decoration:underline;color:#000000';
-//       document.getElementById('select0').style = '';
-//       document.getElementById('select2').style = '';
-//     } else {
-//       document.getElementById('pubs').innerHTML = document.getElementById('pubs_by_topic').innerHTML;
-//       document.getElementById('select2').style = 'text-decoration:underline;color:#000000';
-//       document.getElementById('select0').style = '';
-//       document.getElementById('select1').style = '';
-//     }
-//   }
-
 function showPubs(class_name) {
     if (pub_html == null) {
         pub_html = document.getElementById('pubs').innerHTML;
